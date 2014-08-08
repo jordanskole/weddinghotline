ecbf8353c50a6d982b694a856da132b512cb25b7
Jordan Skole <jordan@getambassador.com>
2014-08-08 11:15:44 -0400

    heroku patch for working dir

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require("express");
 var app = express();
 var moment = require('moment');
+process.env.PWD = process.cwd();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(process.env.PWD + '/public'));
 
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
     console.log('Listening on port %d', server.address().port);
 });
