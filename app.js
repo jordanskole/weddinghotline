3d91f9c40b87f7ceee6b7226e4a227e230bcc846
Jordan Skole <jordan@getambassador.com>
2014-08-08 11:00:56 -0400

    static files

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var app = express();
+var moment = require('moment');
+
+app.use(express.static(__dirname + '/public'));
 
 var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
