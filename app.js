c6d545ea17354c65ceec51e3210283f9f42a184e
Jordan Lewis Skole <jordan@robotcouture.com>
2014-08-11 18:31:24 -0400

    fix thanks route

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.get('/weather', function(req, res){
 app.get('/record', function(req, res){
   // set the response to be an XML document
   res.set('Content-Type', 'text/xml');
-  res.send('<Response><Say>Please leave a recording after the beep. You may press # when finished</Say><Record action="/record/thanks" finishOnKey="#"/><Say>I\'m sorry. I didn\'t hear anything</Say></Response>');
+  res.send('<Response><Say>Please leave a recording after the beep. You may press # when finished</Say><Record method="GET" action="/record/thanks" finishOnKey="#"/><Say>I\'m sorry. I didn\'t hear anything</Say></Response>');
 });
 
 app.get('/record/thanks', function(req, res){
